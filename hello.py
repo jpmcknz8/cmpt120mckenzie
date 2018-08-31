9515fa2cd87b1987892f28c5f172e312a8684fcb
jpmcknz8 <42786531+jpmcknz8@users.noreply.github.com>
2018-08-31 13:43:56 -0400

    Add files via upload

--- hello.py
+++ hello.py
@@ -4,4 +4,11 @@
 def main():
     print ("Hello, instructor!")
 main()
+# Introduction to Programming
+# Author: John Paul McKenzie
+# Date: 8/31/18
+def main ():
+    print ("Hello, instructor!")
+    print ("Good-bye!")
+main ()
 
